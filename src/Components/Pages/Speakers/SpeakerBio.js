c345d5b9deafb16bcb9ae0640c822584422793df
David Bethala <david.bethala@icloud.com>
2018-04-12 10:32:54 -0400

    Stylign

--- src/Components/Pages/Speakers/SpeakerBio.js
+++ src/Components/Pages/Speakers/SpeakerBio.js
@@ -3,28 +3,46 @@ import React from 'react';
 class SpeakerBio extends React.Component {
     render(){
         return (
-            <div className="w3-row" className="individual-speaker">
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1">
-                    <div className="bio-photo-name">
-                        <img src={this.props.photoURL} alt={this.props.altText} /> <h3 className="speaker-name">{this.props.speakerName}</h3>
-                </div>
-                <div className="w3-col l1">
-                    <div className="speaker-desc">
-                        <p>{this.props.speakerDescription}</p>
+            <div className="individual-speaker name-column" >
+                <div className="w3-row">
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1">
+                        <div className="sp-name">
+                            <h3>{this.props.speakerName}</h3>
+                        </div>
                     </div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1 col12"></div>
                 </div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1"></div>
-                <div className="w3-col l1 col12"></div>
+
+                <div className="w3-row">
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1">
+                    </div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1">
+                        <div className="speaker-desc">
+                            <p>{this.props.speakerDescription}</p>
+                        </div>
+                    </div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1"></div>
+                    <div className="w3-col l1 col12"></div>
+                </div>
             </div>
-        </div>
         );
     }
 }
