1a96cdbac1a7bb4f31d12851698021fe4199053e
David Bethala <david.bethala@icloud.com>
2018-04-13 18:43:10 -0400

    Styling

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TitleText from '../Components/Header/TitleText';
 import './Navigation.css';
 
 class Navigation extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.mToggle = this.menuToggle.bind(this);
-        this.state = ({color: 'white'});
+        this.mClose = this.menuClose.bind(this);
+        this.state = {
+            color: this.props.navTextColor
+        };
     }
 
     updateDimensions(){
         if (992 < window.innerWidth) {
             document.getElementsByTagName('nav')[0].style.display = 'block';
+        } else if (992 > window.inmnerWidth) {
+            document.getElementsByClassName('nav-large-screen')
         }
     } /*
     componentWillMount(event) {
@@ -20,17 +26,18 @@ class Navigation extends React.Component {
 
     componentDidMount(event) {
         window.addEventListener("resize", this.updateDimensions);
-        var menuicon = document.getElementsByClassName('mobile-nav-closed')[0];
-        console.log(menuicon);
-        if (menuicon.className === 'mobile-nav-closed-black') {
-            this.setState({color: 'black'});
-            } else { 
-                this.setState({color: 'white'});
-        }
     }
 
     componentWillUnmount(event) {
         window.removeEventListener("resize", this.updateDimensions);
+    }
+
+    menuClose(event) {
+        var navItem = document.getElementsByTagName('nav')[0];
+        if (navItem.style.display === 'block') {
+            navItem.style.display = 'none';
+            navItem.classList.remove('menuhidden');
+        }
     }
     menuToggle (event) {
         var navItem = document.getElementsByTagName('nav')[0];
@@ -38,7 +45,8 @@ class Navigation extends React.Component {
             navItem.style.display = 'none';
         } else {
             navItem.style.display = 'block';
-            navItem.className = navItem.className + ' menu-hidden';
+            navItem.className = navItem.className + ' menuhidden';
+            console.log(this.state);
         }
 
     }
@@ -46,10 +54,13 @@ class Navigation extends React.Component {
     render () {
         return (
             <div className="nav-wrapper">
-                <img className="mobile-nav-closed mobile-nav-closed-black" onClick={this.mToggle} id="mobileNavBlack" src={require('./Assets/menu-black.png')} alt="menu-icon" />
-                <img className="mobile-nav-closed mobile-nav-clsoed-white" onClick={this.mToggle} id="mobileNavWhite" src={require('./Assets/menu-white.png')} alt="menu-icon" />
-                <img className="mobile-nav-open" onClick={this.mToggle} id="mobileNavOpen" src={require('./Assets/close.png')} alt="close-menu" />
+                <img className="mobile-nav-closed" onClick={this.mToggle} id="mobileNavClosed" src={require('./Assets/menu-black.png')} alt="menu-icon" />
+                {<img className="mobile-nav-closed mobile-nav-closed-white" onClick={this.mToggle} id="mobileNavWhite" src={require('./Assets/menu-white.png')} alt="menu-icon" />}
                 <nav className="nav-large-screen">
+                    <div className="mobile-nav-header">
+                        <TitleText navTextColor="white"/>
+                        <img className="mobile-nav-close" onClick={this.mClose} id="mobileNavClose" src={require('./Assets/close.png')} alt="close-menu" style={{zIndex: '3'}}/>
+                    </div>
                     <ul>
                             <li><Link className="header-link to-schedule" to="/schedule" style={{color: this.props.navTextColor}}>Schedule</Link></li>
                             <li><Link className="header-link to-speakers" to="/speakers" style={{color: this.props.navTextColor}}>Speakers</Link></li>
--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -12,7 +12,7 @@ class Register extends React.Component {
         return (
             <div className="registerPage" style={{maxWidth: '1280px', margin: '0 auto'}}>
             <div className="header-region">
-                <Header />
+                <Header navTextColor="black"/>
                 </div>
 
                 <div className="w3-row register-links-row">
